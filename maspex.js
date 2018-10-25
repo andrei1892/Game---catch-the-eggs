b880cf62e671b2f3f05cd0377a9e965769cf4d4b
Andrei <39693794+andrei1892@users.noreply.github.com>
2018-10-25 23:20:04 +0300

    Add files via upload

--- maspex.js
+++ maspex.js
@@ -3,22 +3,22 @@ var basket = document.getElementById("basket");
 var game = document.getElementById("gameContainer");
 var right = false;
 var left = false;
-var basketX = (container.offsetWidth-basket.offsetWidth)/2; //basket position
+var basketX; //  (container.offsetWidth-basket.offsetWidth)/2; 
 var lifes = document.getElementById("lifes");
 var score = document.getElementById("score");
 var time = document.getElementById("time");
 var lifePoints = 20; // starting lifes value;
-var scorePoints = 0 ; //starting score
+var scorePoints = 0; //starting score
 var runGame;
 var timer = 60;
-var classes = ["egg1","egg2","egg3","egg4","egg5","rock1","rock2","egg3","rock3","heart","time","egg1"];
+var classes = ["egg1", "egg2", "egg3", "egg4", "egg5", "rock1", "rock2", "egg3", "rock3", "heart", "time", "egg1"];
 
-function countdown(){	
+function countdown() {	
 		var startDate = Date.now();
 		var endDate = startDate  + 10000 //60000;
 		var timer1 = parseInt(( endDate - startDate ) /  1000);
 		timer = 20; //60;
-		let counter = setInterval(function(){
+		let counter = setInterval(function() {
 				                if(timer > 0) { timer --;
 												document.getElementById("time").innerHTML = timer;
 												}	             
@@ -53,36 +53,38 @@ function keyUpHandler(e) { // event handler 2
 
 function moveBasket() {
     if(right && basketX <= container.offsetWidth-basket.offsetWidth ) {
-        basketX += 10; // moving problem
+        basketX += 20; 
         basket.style.left = basketX + "px";
     }
     else if(left && basketX >= 0 ) {
-        basketX -= 10;
+        basketX -= 20;
         basket.style.left = basketX + "px";
     }  
 };
 
-function intersection(fallingElement, theBasket){ 
+function intersection(fallingElement, theBasket) { 
     let fallingElementCoord = fallingElement.getBoundingClientRect();
     let basketCoord = theBasket.getBoundingClientRect();
-    if( fallingElementCoord.left  >= basketCoord.left + 20 && 
-        fallingElementCoord.right <= basketCoord.right - 20) {
-        return true;
+    if( fallingElementCoord.left >= basketCoord.left + 20 && fallingElementCoord.right <= basketCoord.right - 20) {
+                                                                                                                        return true;
+            }
+    else {    
+            return false;
     }
-    else {      return false;}
 }
 
-function spawn(){
+function spawn() {
 	let x = Math.floor(Math.random() * 12)   //  x will be the random index in the array holding the falling elements type - eggs, rocks and life 
 	let type = classes[x];
     let newElement = document.createElement("div");
     let newElementX;
     newElement.className = "fallingElement"; 
 	newElement.style.backgroundImage = "url(" + type + ".png)";
-    newElement.style.left = Math.floor(Math.random()* container.offsetWidth - newElement.offsetWidth + 1) + "px";
+    newElement.style.left = 20 + Math.floor(Math.random() * (container.offsetWidth - 55 ) ) + "px";    // newElement.offsetWidth = 35 ; using a limit of 20px on each side
     newElement.style.transition = "transform " + 3500 + "ms linear"; // 3500 -> transition time -> variable
-    container.appendChild(newElement);
-    setTimeout(function(){  newElement.classList.add ( "move" ) ;  // add transform to the transition -> perform falling
+    game.appendChild(newElement);
+    setTimeout(function(){ 
+                            newElement.classList.add ( "move" ) ;  // add transform to the transition -> perform falling
                             setTimeout(function(){  if( intersection(newElement, basket ) ) { // check which element is intersecting with the basket
 																				           if( /egg/.test(type) ) {
 																					                              		scorePoints++; 
@@ -111,8 +113,8 @@ function spawn(){
                                                                         endGame();
                                                                         clearInterval(counter);
 														                      }                
-                                         },3220); // the moment of the evaluation of the falling element's coord;
-                     }, 1000 );      
+                                         },3220);// evaluate the falling element's coord at this specific time - which is the moment of the intersection;
+                     }, 800 );      
     // considering full distance 650 px, and 600px meeting point with basket -> time elapsed till the meeting point would be 92,59% of the full time of the transition ( or 100/(d1/d2) if we change distances ) ; 
    $(newElement).one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(event) { 
                     $(this).remove();
@@ -135,6 +137,7 @@ $('#initialize').click(function() {
     document.addEventListener("keyup", keyUpHandler, false);  
     $('#score').html(scorePoints);
     $('#lifes').html(lifePoints);
+    basketX = (container.offsetWidth-basket.offsetWidth)/2;
     countdown(); 
     runGame = setInterval( function(){   // set the eggs falling at 1 second
                                       for (i = 0; i < 1; i++) { 
